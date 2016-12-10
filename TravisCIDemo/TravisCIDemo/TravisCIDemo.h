e75b5b442d0350516e13e7cb175b0f62f839dcc5
wangxiaoxiang <wangxiaoxiang@alibaba-inc.com>
2016-12-10 14:31:10 +0800

    init

---
+++ TravisCIDemo/TravisCIDemo/TravisCIDemo.h
@@ -0,0 +1,19 @@
+//
+//  TravisCIDemo.h
+//  TravisCIDemo
+//
+//  Created by 汪潇翔 on 10/12/2016.
+//  Copyright © 2016 汪潇翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TravisCIDemo.
+FOUNDATION_EXPORT double TravisCIDemoVersionNumber;
+
+//! Project version string for TravisCIDemo.
+FOUNDATION_EXPORT const unsigned char TravisCIDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TravisCIDemo/PublicHeader.h>
+
+
